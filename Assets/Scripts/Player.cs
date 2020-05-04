7b075f07d6ac70faa22f741d89319598aa99377c
Danielle <dlkogan22@gmail.com>
2020-05-04 19:37:16 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 3.5f;
+    void Start()
+    {
+        transform.position = new Vector3(0, -4.89f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calculateMovement();
+    }
+    void calculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        float maxY = 4.9f;
+        float minY = -4.89f;
+        float maxX = 11.3f;
+        float minX = -11.3f;
+        if (transform.position.y >= maxY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY, 0);
+        }
+        else if (transform.position.y <= minY)
+        {
+            transform.position = new Vector3(transform.position.x, minY, 0);
+        }
+
+        if (transform.position.x >= maxX)
+        {
+            transform.position = new Vector3(minX, transform.position.y, 0);
+        }
+        else if (transform.position.x < minX)
+        {
+            transform.position = new Vector3(maxX, transform.position.y, 0);
+        }
+    }
+}
